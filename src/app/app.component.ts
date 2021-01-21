9b822595cdea424533fb55afb1e00b4ff3ee7378
Augusto Ciuffoletti <augusto.ciuffoletti@gmail.com>
2021-01-21 18:18:03 +0100

    Step_07 Semplificazione della tabella delle città (senza temperature)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,31 +8,18 @@ import { WeatherbitService } from './weatherbit.service';
 })
 export class AppComponent {
   title = 'Esercizio Angular';
-  cities = [
-    {
-      nome: 'Torino',
-      temperatura: '14'
-    }, {
-      nome: 'Milano',
-      temperatura: '15'
-    }, {
-      nome: 'Genova',
-      temperatura: '18'
-    }
-  ];
-  selezionata: any;
+  cities = ['Torino','Milano','Genova'];
+  selezionata: string;
+  temperatura: string;
   constructor(private wbs: WeatherbitService) { }
   clean() {
     this.selezionata=undefined;
   }
   refreshTemperature(itemName: string) {
-    var trovato = this.cities.filter(
-      el => ( el.nome === itemName )
-    );
-    this.selezionata = trovato[0];
-    this.wbs.getData(this.selezionata.nome).subscribe(
-      x => this.selezionata.temperatura = x.data[0].temp,
+    this.selezionata=itemName;
+    this.wbs.getData(itemName).subscribe(
+      x => this.temperatura = x.data[0].temp,
       err => console.error('Observer got an error: ' + err)
     );
- }
+  }
 }
